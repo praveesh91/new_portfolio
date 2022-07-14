631ca267705e19d42ee250f8fb909beb696135b2
praveesh91 <praveesh91@gmail.com>
2022-07-14 11:11:39 +0530

    sentry integration

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import "./i18n";
 import ThemeContextWrapper from "./themeContextWrapper";
+
+Sentry.init({
+  dsn: "https://67b53c08e45840f2b9774890e266e565@o1318155.ingest.sentry.io/6572215",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
